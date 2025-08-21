41f1ad1b9da9939f0ce98a3b9fbcdffb22010459
Rabab Korikache <korikacherabab@gmail.com>
2025-08-21 18:52:50 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello and welcome!");
+
+            // wait for 2 seconds (2000 milliseconds)
+            Thread.sleep(2000);
+        }
+    }
+}
